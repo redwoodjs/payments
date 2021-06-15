86f9d81898466d64cc31210d62480c2848a29833
chrisvdm <nymanchristine@gmail.com>
2021-06-15 20:11:09 +0200

    create createCheckoutSession function

---
+++ sandbox/api/src/functions/createCheckoutSession.js
@@ -0,0 +1,54 @@
+import { logger } from 'src/lib/logger'
+
+const stripe = require('stripe')(process.env.STRIPE_SK_TEST)
+
+/**
+ * The handler function is your code that processes http request events.
+ * You can use return and throw to send a response or error, respectively.
+ *
+ * Important: When deployed, a custom serverless function is an open API endpoint and
+ * is your responsibility to secure appropriately.
+ *
+ * @see {@link https://redwoodjs.com/docs/serverless-functions#security-considerations|Serverless Function Considerations}
+ * in the RedwoodJS documentation for more information.
+ *
+ * @typedef { import('aws-lambda').APIGatewayEvent } APIGatewayEvent
+ * @typedef { import('aws-lambda').Context } Context
+ * @param { APIGatewayEvent } event - an object which contains information from the invoker.
+ * @param { Context } context - contains information about the invocation,
+ * function, and execution environment.
+ */
+export const handler = async (event, context) => {
+  logger.info('Invoked createCheckoutSession function')
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'Stubborn Attachments',
+            images: ['https://i.imgur.com/EHyR2nP.png'],
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `http://localhost:8910/success.html`,
+    cancel_url: `http://localhost:8910/cancel.html`,
+  });
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.STRIPE_SK_TEST}`
+    },
+
+    body: JSON.stringify({
+      data: session
+    })
+  }
+}
