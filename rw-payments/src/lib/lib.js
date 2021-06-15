1b00364751dd88470db92a7f11444db22b0cd6db
chrisvdm <nymanchristine@gmail.com>
2021-06-15 22:59:34 +0200

    create semi abstraction of prebuilt checkout

--- rw-payments/src/lib/lib.js
+++ rw-payments/src/lib/lib.js
@@ -1,3 +1,20 @@
-exports.aFn = () => console.log('I am a lib fn')
+const {loadStripe} = require("@stripe/stripe-js")
 
-exports.anotherFn = () => console.log('I am another fn.')
+const stripePromise = loadStripe(process.env.STRIPE_PK_TEST)
+
+exports.redirectCheckout = async (payload) => {
+    console.log('Entering package...')
+    const stripe = await stripePromise;
+    const response = await global.fetch(`${global.__REDWOOD__API_PROXY_PATH}/createCheckoutSession`, {
+        method: "POST",
+        body: JSON.stringify(payload)
+    })
+    const session = await response.json()
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id
+    });
+
+   if (result.error) {
+     console.log(result.error.message)
+   }
+}
