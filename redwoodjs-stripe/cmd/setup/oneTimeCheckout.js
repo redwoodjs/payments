ac911b1ba1f50bb4aee41c28e220b709a8d22ecf
chrisvdm <nymanchristine@gmail.com>
2021-09-01 19:53:18 +0200

    templating structure (wip)

---
+++ redwoodjs-stripe/cmd/setup/oneTimeCheckout.js
@@ -0,0 +1,20 @@
+/*
+1.  Check for products on account 
+        OR
+    await stripe.prices.create({
+        product_data: {
+            name: 'Potatoes 2kg'
+        },
+        currency: 'usd',
+        unit_amount: 800
+    });
+    await stripe.prices.create({
+        product_data: {
+            name: 'Strawberry tray'
+        },
+        currency: 'usd',
+        unit_amount: 500
+    });
+2. yarn rw g page cart
+3. Add CartPage markup
+*/
---
+++ redwoodjs-stripe/templates/handlers.js
@@ -0,0 +1,3 @@
+const handleCartPageTemplating = ({ mode = 'oneTime'}) => {
+   
+}
