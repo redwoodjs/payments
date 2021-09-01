de7b6416d2b85d841dcefd82fc6838c703ea8ff9
chrisvdm <nymanchristine@gmail.com>
2021-09-01 21:38:58 +0200

    exploring appending CLI commands

---
+++ app/foo.js
@@ -0,0 +1,12 @@
+const bar = () => {
+  console.log("===============================================")
+  console.log("I am running in the background")
+//   const Readable = require('stream').Readable;
+// const s = new Readable();
+// s._read = () => {}; // redundant? see update below
+// s.push('your text here');
+// s.push(null);
+  console.log("===============================================")
+}
+
+bar()
--- app/pluginSetup.js
+++ app/pluginSetup.js
@@ -1,14 +1,16 @@
 const readline = require("readline");
 
 const pluginSetup = () => {
-  console.log("Installing redwoodjs-stripe package")
-  console.log("(Not really but it will)")
-  console.log('We are going to ask you a few questions about your Stripe account')
   const config = {}
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
+
+  console.log("Installing redwoodjs-stripe package")
+  console.log("(Not really but it will)")
+  console.log('We are going to ask you a few questions about your Stripe account')
+
   rl.question("What is your Stripe secret test key? ", function (stripeSK) {
     rl.question("What is your Stripe public test key? ", function (stripePK) {
       rl.question('What is your Stripe Webhook secret key?', (stripeWSK) => {
@@ -16,9 +18,8 @@ const pluginSetup = () => {
         config['STRIPE_PK'] = stripePK
         config['STRIPE_WEBHOOK_SK'] = stripeWSK
         rl.close()
-    })
       })
-
+    })
   })
 
   rl.on("close", function() {
