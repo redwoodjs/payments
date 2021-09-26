409711ae5c951ad8b3228335a054270b58e67f82
chrisvdm <nymanchristine@gmail.com>
2021-09-26 19:49:58 +0200

    configure customer portal on subscription setup

---
+++ app/plugin/stripe/setupSubscriptionCheckout.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SK)
+// process.env.STRIPE_SK
+const subscriptionCheckoutSetup = async () => {
+  // Create products
+
+  // Send priceID array
+
+    // TODO: Add prompt to ask to configure Customer Portal
+    const configuration = await stripe.billingPortal.configurations.create({
+   business_profile: {
+      privacy_policy_url: 'https://example.com/privacy',
+      terms_of_service_url: 'https://example.com/terms',
+    },
+    features: {
+      invoice_history: {
+        enabled: true,
+      },
+      "payment_method_update": {
+        "enabled": false
+      },
+      "subscription_cancel": {
+        "cancellation_reason": {
+          "enabled": false,
+          "options": ["too_expensive", "other"]
+        },
+        "enabled": false,
+        "mode": "at_period_end",
+        "proration_behavior": "none"
+      },
+      "subscription_update": {
+        "default_allowed_updates": ["price"],
+        "products": [
+          {
+            prices: ['price_1JdarsFnEy6nnTnpxOI5D4fA'],
+            product: 'prod_KIBEs5N3qDXwAb'  },
+          {
+            prices: ['price_1JdattFnEy6nnTnpW5XYcieQ'],
+            product: 'prod_KIBGMCHJlTW9ff'
+          }
+        ],
+        "enabled": false,
+        "proration_behavior": "none"
+      }
+    }
+});
+
+
+  console.log("PORTAL CONFIG:", configuration)
+}
+
+subscriptionCheckoutSetup()
--- app/web/src/pages/StripeCartPage/StripeCartPage.js
+++ app/web/src/pages/StripeCartPage/StripeCartPage.js
@@ -23,22 +23,22 @@ const StripeCartPage = () => {
   }, [success])
 
   return (
+
     <>
       <MetaTags
         title="Stripe Cart"
         description="A cart page for trying out the Stripe checkout"
-
       />
 
       <h2>Stripe Cart</h2>
       <p>Click the checkout button below to try out the Stripe Checkout</p>
       <ul className='cart-drop-down__list list--no-style'>
-      <li className='cart-drop-down__list__item'>Item 1</li>
-      <li className='cart-drop-down__list__item'>Item 2</li>
-    </ul>
-    <button onClick={onCheckoutButtonClick}>
-      Checkout
-    </button>
+        <li className='cart-drop-down__list__item'>Item 1</li>
+        <li className='cart-drop-down__list__item'>Item 2</li>
+      </ul>
+      <button onClick={onCheckoutButtonClick}>
+        Checkout
+      </button>
     </>
   )
 }
